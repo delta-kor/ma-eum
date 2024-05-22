9886541bea5b6b04347ccdb69ae361d151c7781b
delta-kor <deltakbot1@gmail.com>
2024-05-22 17:53:26 +0900

    feat: add Icon

--- components/core/Header.tsx
+++ components/core/Header.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import ArtisticText from '@/components/core/ArtisticText';
+import Icon from '@/components/core/Icon';
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between p-24">
+    <div className="flex items-center justify-between bg-black p-24">
       <ArtisticText type="maeum" className="h-24" />
+      <Icon type="search" className="w-24 text-primary-500" />
     </div>
   );
 }
