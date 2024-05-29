26a217bae452d393bfe93c2003398829014a2a0d
delta-kor <deltakbot1@gmail.com>
2024-05-29 16:26:06 +0900

    fix: prevent video cards being too big

--- components/card/AlbumCard.tsx
+++ components/card/AlbumCard.tsx
@@ -15,16 +15,16 @@ export default function AlbumCard({ album, menu }: Props) {
   return (
     <div
       style={{ background: `linear-gradient(101deg, ${gradientFrom} 7.15%, ${gradientTo} 96.7%)` }}
-      className="flex flex-col gap-12 rounded-16 p-16"
+      className="flex flex-col gap-12 rounded-16 p-12"
     >
       <div className="flex items-center gap-12">
         <img
           alt={album.title}
           src={ImageUrl.album(album.id)}
-          className="size-[74px] rounded-8 border-3 border-white/40"
+          className="size-64 rounded-8 border-3 border-white/40"
         />
         <div className="flex flex-col gap-4">
-          <div className="text-24 font-700 text-white">{album.title}</div>
+          <div className="text-20 font-700 text-white">{album.title}</div>
           <div className="text-16 font-500 text-white opacity-70">
             {format(album.release, 'yy. MM. dd.')}
           </div>
@@ -34,17 +34,21 @@ export default function AlbumCard({ album, menu }: Props) {
         <div className="relative -mx-8 flex h-32 items-stretch">
           <Link
             href={`/albums/${album.id}/promotion`}
-            className="flex grow basis-0 cursor-pointer items-center justify-center text-center text-18 font-500 text-white"
+            className="flex grow basis-0 cursor-pointer items-center justify-center text-center text-16 font-500 text-white"
           >
             <Translate>$promotion</Translate>
           </Link>
-          <Link
-            href={`/albums/${album.id}/performance`}
-            className="flex grow basis-0 cursor-pointer items-center justify-center text-center text-18 font-500 text-white"
-          >
-            <Translate>$performance</Translate>
-          </Link>
-          <div className="absolute left-1/2 h-full w-2 -translate-x-1/2 bg-white opacity-30" />
+          {!album.isMini && (
+            <>
+              <Link
+                href={`/albums/${album.id}/performance`}
+                className="flex grow basis-0 cursor-pointer items-center justify-center text-center text-16 font-500 text-white"
+              >
+                <Translate>$performance</Translate>
+              </Link>
+              <div className="absolute left-1/2 h-full w-2 -translate-x-1/2 bg-white opacity-30" />
+            </>
+          )}
         </div>
       )}
     </div>
--- components/card/ProgramCard.tsx
+++ components/card/ProgramCard.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export default function ProgramCard({ category }: Props) {
   return (
-    <div className="flex grow basis-0 cursor-pointer items-center gap-10 rounded-16 border-3 border-primary-200 bg-primary-100 px-24 py-16">
-      <div className="flex w-full flex-col gap-6">
-        <div className="whitespace-nowrap text-18 font-700 text-black">{category.title}</div>
+    <div className="flex grow basis-0 cursor-pointer items-center gap-10 rounded-16 border-3 border-primary-200 bg-primary-100 px-16 py-12">
+      <div className="flex w-full flex-col gap-4">
+        <div className="whitespace-nowrap text-16 font-700 text-black">{category.title}</div>
         <div className="whitespace-nowrap text-14 font-500 text-primary-500">
           {category.description}
         </div>
