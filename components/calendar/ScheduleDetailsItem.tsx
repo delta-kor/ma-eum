35128c28ff323377cc42f03c0b1b86c8aa66305a
delta-kor <deltakbot1@gmail.com>
2024-06-03 13:21:53 +0900

    feat: add schedule url

--- components/calendar/ScheduleDetailsItem.tsx
+++ components/calendar/ScheduleDetailsItem.tsx
@@ -1,4 +1,5 @@
 import ScheduleTypeChip from '@/components/calendar/ScheduleTypeChip';
+import Icon from '@/components/core/Icon';
 import Translate from '@/components/core/Translate';
 import { Schedule } from '@prisma/client';
 
@@ -8,14 +9,22 @@ interface Props {
 
 export default function ScheduleDetailsItem({ schedule }: Props) {
   return (
-    <div className="flex items-center gap-16 rounded-16 border-3 border-gray-100 px-24 py-16">
-      <ScheduleTypeChip kind="icon" type={schedule.type} />
-      <div className="flex flex-col gap-4">
-        <div className="text-16 font-500 text-gray-500">
-          <Translate>{schedule.isAllDay ? '$all_day' : schedule.dateDetails!}</Translate>
+    <div className="flex flex-col gap-12 rounded-16 border-3 border-gray-100 px-24 py-16">
+      <div className="flex items-center gap-16">
+        <ScheduleTypeChip kind="icon" type={schedule.type} />
+        <div className="flex min-w-0 flex-col gap-4">
+          <div className="text-16 font-500 text-gray-500">
+            <Translate>{schedule.isAllDay ? '$all_day' : schedule.dateDetails!}</Translate>
+          </div>
+          <div className="text-18 font-600 text-black">{schedule.title}</div>
         </div>
-        <div className="text-18 font-600 text-black">{schedule.title}</div>
       </div>
+      {schedule.url && (
+        <a href={schedule.url} target="_blank" className="flex items-center gap-8">
+          <Icon type="link" className="w-14 text-gray-500" />
+          <div className="truncate text-14 font-600 text-gray-500 underline">{schedule.url}</div>
+        </a>
+      )}
     </div>
   );
 }
