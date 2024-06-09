818e1a66dc588512343b4eaa1ab08445a4e2a511
delta-kor <deltakbot1@gmail.com>
2024-06-09 22:05:50 +0900

    feat: add stage video member filter

--- components/video/StageVideoList.tsx
+++ components/video/StageVideoList.tsx
@@ -12,6 +12,7 @@ export default function StageVideoList() {
 
   const sessions = trpc.session.getSessionsByMusicId.useQuery(
     {
+      member: null,
       musicId: selectedMusicId!,
     },
     {
--- services/session.service.ts
+++ services/session.service.ts
@@ -2,13 +2,14 @@ import prisma from '@/prisma/prisma';
 import { publicProcedure, router } from '@/trpc/router';
 import { ControlledCache, StaticDataTtl } from '@/utils/cache.util';
 import { ExtendedSession } from '@/utils/session.util';
+import { Member } from '@/utils/video.util';
 import { z } from 'zod';
 
 const SessionRouter = router({
   getSessionsByMusicId: publicProcedure
-    .input(z.object({ musicId: z.string() }))
+    .input(z.object({ member: z.string().nullable(), musicId: z.string() }))
     .query(async opts => {
-      return SessionService.getSessionsByMusicId(opts.input.musicId);
+      return SessionService.getSessionsByMusicId(opts.input.musicId, opts.input.member as Member);
     }),
 });
 
@@ -16,17 +17,33 @@ export class SessionService {
   public static router = SessionRouter;
 
   @ControlledCache('session.getSessionsByMusicId', StaticDataTtl)
-  public static async getSessionsByMusicId(musicId: string): Promise<ExtendedSession[]> {
-    return prisma.session.findMany({
-      include: {
-        videos: true,
-      },
-      orderBy: {
-        date: 'asc',
-      },
-      where: {
-        musicId,
-      },
-    });
+  public static async getSessionsByMusicId(
+    musicId: string,
+    member?: Member | null
+  ): Promise<ExtendedSession[]> {
+    if (typeof member === 'undefined')
+      return prisma.session.findMany({
+        include: {
+          videos: true,
+        },
+        orderBy: {
+          date: 'asc',
+        },
+        where: {
+          musicId,
+        },
+      });
+
+    const sessions = await SessionService.getSessionsByMusicId(musicId);
+    for (const session of sessions) {
+      session.videos = session.videos.filter(video =>
+        member === null
+          ? video.meta.every(meta => meta.type !== 'members')
+          : video.meta.some(meta => meta.type === 'members' && meta.members.includes(member))
+      );
+    }
+
+    const filteredSessions = sessions.filter(session => session.videos.length > 0);
+    return filteredSessions;
   }
 }
