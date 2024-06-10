83c0a6332f9d7749832135e42be0723b77c1bdb2
delta-kor <deltakbot1@gmail.com>
2024-06-10 14:40:05 +0900

    fix: prevent link looping

--- components/card/MusicCardItem.tsx
+++ components/card/MusicCardItem.tsx
@@ -9,6 +9,7 @@ export default function MusicCardItem({ music }: Props) {
   return (
     <Link
       href={`/play/${music.id}`}
+      replace
       className="flex cursor-pointer items-center gap-16 px-16 py-12"
     >
       <div className="w-10 shrink-0 text-18 font-600 text-primary-500">{music.order}</div>
