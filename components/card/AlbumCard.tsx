969a5b423ba6324b6fa2ec17d8092f405727447d
delta-kor <deltakbot1@gmail.com>
2024-06-24 13:50:01 +0900

    fix: remove motion wrapper from AlbumCard component

--- components/card/AlbumCard.tsx
+++ components/card/AlbumCard.tsx
@@ -1,5 +1,4 @@
 import LazyImage from '@/components/core/LazyImage';
-import MotionWrapper from '@/components/core/MotionWrapper';
 import Translate from '@/components/core/Translate';
 import { ImageUrl } from '@/utils/url.util';
 import { Album } from '@prisma/client';
@@ -15,40 +14,25 @@ export default function AlbumCard({ album, menu }: Props) {
   const [gradientFrom, gradientTo] = album.colors;
 
   return (
-    <MotionWrapper
-      layoutId={`album-card-wrapper-${album.id}`}
+    <div
       style={{ background: `linear-gradient(101deg, ${gradientFrom} 7.15%, ${gradientTo} 96.7%)` }}
-      size
       className="flex flex-col gap-12 rounded-16 p-12"
     >
       <div className="flex items-center gap-12">
-        <MotionWrapper layoutId={`album-card-image-${album.id}`} size>
-          <LazyImage
-            alt={album.title}
-            src={ImageUrl.album(album.id)}
-            className="size-64 rounded-8 border-3 border-white/40 bg-white/40"
-          />
-        </MotionWrapper>
+        <LazyImage
+          alt={album.title}
+          src={ImageUrl.album(album.id)}
+          className="size-64 rounded-8 border-3 border-white/40 bg-white/40"
+        />
         <div className="flex flex-col gap-4">
-          <MotionWrapper
-            layoutId={`album-card-title-${album.id}`}
-            className="text-20 font-700 text-white"
-          >
-            {album.title}
-          </MotionWrapper>
-          <MotionWrapper
-            layoutId={`album-card-description-${album.id}`}
-            className="text-16 font-500 text-white/70"
-          >
+          <div className="text-20 font-700 text-white">{album.title}</div>
+          <div className="text-16 font-500 text-white/70">
             {format(album.release, 'yy. MM. dd.')}
-          </MotionWrapper>
+          </div>
         </div>
       </div>
       {menu && (
-        <MotionWrapper
-          layoutId={`album-card-menu-${album.id}`}
-          className="relative -mx-8 flex h-32 items-stretch"
-        >
+        <div className="relative -mx-8 flex h-32 items-stretch">
           {!album.isMini && (
             <>
               <Link
@@ -66,8 +50,8 @@ export default function AlbumCard({ album, menu }: Props) {
           >
             <Translate>$promotion</Translate>
           </Link>
-        </MotionWrapper>
+        </div>
       )}
-    </MotionWrapper>
+    </div>
   );
 }
