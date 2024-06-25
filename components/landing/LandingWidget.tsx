bc0b508c3c2b668529d039f98fc2ce82d3feaae5
delta-kor <deltakbot1@gmail.com>
2024-06-25 21:12:25 +0900

    fix: LandingWidget d-day

--- components/landing/LandingWidget.tsx
+++ components/landing/LandingWidget.tsx
@@ -6,7 +6,7 @@ import { DateTime } from 'luxon';
 export default function LandingWidget() {
   const today = DateTime.local({ zone: 'Asia/Seoul' });
   const debut = DateTime.local(2022, 7, 27, { zone: 'Asia/Seoul' });
-  const day = Math.floor(today.diff(debut, 'days').days);
+  const day = Math.floor(today.diff(debut, 'days').days) + 1;
 
   return (
     <div className="absolute bottom-0 left-1/2 flex -translate-x-1/2 translate-y-1/2 items-center gap-12 rounded-16 bg-gradient-primary px-16 py-8 shadow-primary lg:static lg:translate-x-0 lg:translate-y-0">
