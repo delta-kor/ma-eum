d8a1797fb39ba63e49f1beac45811b0eaf7a74c9
delta-kor <deltakbot1@gmail.com>
2024-06-30 21:34:19 +0900

    feat: add talk article write, edit loading page

---
+++ app/(main)/(header)/(details)/talk/article/[articleId]/edit/loading.tsx
@@ -0,0 +1,35 @@
+import Translate from '@/components/core/Translate';
+import DetailsContent from '@/components/core/header/DetailsContent';
+import Title from '@/components/core/header/Title';
+
+export default function TalkEditLoading() {
+  return (
+    <DetailsContent>
+      <Title>Edit</Title>
+      <div className="px-24">
+        <div className="pb-24 pt-16 lg:mx-auto lg:max-w-screen-lg lg:pt-24">
+          <div className="flex flex-col gap-16">
+            <div className="flex items-center gap-16">
+              <div className="flex min-w-0 grow animate-pulse flex-col gap-8">
+                <div className="h-[29px] w-full max-w-[512px] rounded-4 bg-gray-100" />
+                <div className="h-[21px] w-[86px] rounded-4 bg-gray-100" />
+              </div>
+              <button className="group flex cursor-not-allowed items-center gap-8 rounded-8 bg-gray-100 px-16 py-8">
+                <div className="text-16 font-600 text-gray-200">
+                  <Translate>$talk_article_edit</Translate>
+                </div>
+              </button>
+            </div>
+            <div className="h-2 bg-gray-100" />
+            <div className="flex min-h-[240px] flex-col gap-8">
+              <div className="h-[19px] w-full max-w-[628px] rounded-4  bg-gray-50" />
+              <div className="h-[19px] w-full max-w-[628px] rounded-4 bg-gray-50" />
+              <div className="h-[19px] w-3/5 max-w-[480px] rounded-4 bg-gray-50" />
+              <div className="h-[19px] w-1/5 max-w-[240px] rounded-4 bg-gray-50" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </DetailsContent>
+  );
+}
