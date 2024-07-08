c60e852db7fa676d043b9d7e1b9608b96a3b5817
delta-kor <deltakbot1@gmail.com>
2024-07-09 00:05:33 +0900

    feat: add click icon to AlbumCard component

--- components/card/AlbumCard.tsx
+++ components/card/AlbumCard.tsx
@@ -1,3 +1,4 @@
+import Icon from '@/components/core/Icon';
 import LazyImage from '@/components/core/LazyImage';
 import { formatTimeAsDate } from '@/utils/time.util';
 import { ImageUrl } from '@/utils/url.util';
@@ -13,21 +14,21 @@ export default function AlbumCard({ album, menu }: Props) {
   const [gradientFrom, gradientTo] = album.colors;
 
   return (
-    <div
+    <Link
+      href={`/videos/albums/${album.id}/performance`}
       style={{ background: `linear-gradient(101deg, ${gradientFrom} 7.15%, ${gradientTo} 96.7%)` }}
-      className="flex flex-col gap-12 rounded-16 p-12"
+      className="flex items-center gap-12 rounded-16 py-12 pl-12 pr-16"
     >
-      <Link href={`/videos/albums/${album.id}/performance`} className="flex items-center gap-12">
-        <LazyImage
-          alt={album.title}
-          src={ImageUrl.album(album.id)}
-          className="size-64 rounded-8 border-3 border-white/40 bg-white/40"
-        />
-        <div className="flex flex-col gap-4">
-          <div className="text-20 font-700 text-white">{album.title}</div>
-          <div className="text-16 font-500 text-white/70">{formatTimeAsDate(album.release)}</div>
-        </div>
-      </Link>
-    </div>
+      <LazyImage
+        alt={album.title}
+        src={ImageUrl.album(album.id)}
+        className="size-64 shrink-0 rounded-8 border-3 border-white/40 bg-white/40"
+      />
+      <div className="flex grow flex-col gap-4">
+        <div className="text-20 font-700 text-white">{album.title}</div>
+        <div className="text-16 font-500 text-white/70">{formatTimeAsDate(album.release)}</div>
+      </div>
+      {menu && <Icon type="right" className="w-14 shrink-0 text-white/50" />}
+    </Link>
   );
 }
