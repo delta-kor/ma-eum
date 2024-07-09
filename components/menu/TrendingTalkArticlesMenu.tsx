caaca11b2396487d411a4ff244647634a9ed1615
delta-kor <deltakbot1@gmail.com>
2024-07-09 20:48:00 +0900

    feat: add translation to trending talk articles

--- components/menu/TrendingTalkArticlesMenu.tsx
+++ components/menu/TrendingTalkArticlesMenu.tsx
@@ -1,3 +1,4 @@
+import Translate from '@/components/core/Translate';
 import TrendingTalkArticlesMenuItem from '@/components/menu/TrendingTalkArticlesMenuItem';
 import { TalkService } from '@/services/talk.service';
 import Link from 'next/link';
@@ -9,9 +10,11 @@ export default async function TrendingTalkArticlesMenu() {
     <div className="flex flex-col gap-12">
       <div className="flex items-center gap-8">
         <div className="shrink-0 text-18 font-700 text-black">MAEUM TALK</div>
-        <div className="grow text-18 font-600 text-primary-500">인기글</div>
+        <div className="grow text-18 font-600 text-primary-500">
+          <Translate>$talk_article_trending</Translate>
+        </div>
         <Link href={`/talk`} className="shrink-0 text-16 font-500 text-gray-500">
-          전체보기
+          <Translate>$view_all</Translate>
         </Link>
       </div>
       <div className="flex flex-col gap-12">
