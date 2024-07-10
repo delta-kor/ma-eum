12c992cabd19628cb676c5cf252450518439266d
delta-kor <deltakbot1@gmail.com>
2024-07-10 18:37:53 +0900

    fix: settings header project version

--- components/settings/SettingsHeader.tsx
+++ components/settings/SettingsHeader.tsx
@@ -1,10 +1,11 @@
 import ArtisticText from '@/components/core/ArtisticText';
+import { version } from '@/package.json';
 
 export default function SettingsHeader() {
   return (
     <div className="flex flex-col items-center gap-12">
       <ArtisticText type="maeum" className="h-24 text-primary-500" />
-      <div className="text-20 font-500 text-gray-500">v{process.env.npm_package_version}</div>
+      <div className="text-20 font-500 text-gray-500">v{version}</div>
     </div>
   );
 }
