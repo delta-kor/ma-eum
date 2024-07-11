aeb61dc6bcbe3fdc360eb15c796beaa5f220d883
delta-kor <deltakbot1@gmail.com>
2024-07-11 12:43:37 +0900

    feat: add i18n japanese support

--- utils/i18n.util.ts
+++ utils/i18n.util.ts
@@ -1,15 +1,21 @@
 import en from '@/locales/en.json';
+import jp from '@/locales/jp.json';
 import ko from '@/locales/ko.json';
+
+function getTranslation(key: string, locale: any): null | string {
+  if (key[0] !== '$') return null;
+
+  const slicedKey = key.slice(1);
+  if (slicedKey in locale) return locale[slicedKey];
+
+  return null;
+}
 
 export function i18n(key: string, lang: string): string {
   let locales: any;
   if (lang === 'ko') locales = ko;
-  else if (lang === 'en') locales = en;
+  else if (lang === 'jp') locales = jp;
   else locales = en;
 
-  if (key[0] === '$') {
-    const slicedKey = key.slice(1);
-    if (slicedKey in locales) return locales[slicedKey];
-    return `${slicedKey}`;
-  } else return key;
+  return getTranslation(key, locales) || getTranslation(key, en) || getTranslation(key, jp) || key;
 }
