610249c8cd5f1da361a1efc568f0671035fc7f37
delta-kor <deltakbot1@gmail.com>
2024-07-20 19:55:18 +0900

    feat: add adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import TRPCProvider from '@/providers/TRPCProvider';
 import TranslateProvider from '@/providers/TranslateProvider';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 import './globals.css';
 
@@ -9,7 +10,15 @@ interface Props {
 
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang="ko">
+    <html lang="en">
+      <head>
+        <Script
+          crossOrigin="anonymous"
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+          strategy="lazyOnload"
+          async
+        ></Script>
+      </head>
       <body>
         <TRPCProvider>
           <TranslateProvider>{children}</TranslateProvider>
