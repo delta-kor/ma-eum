b1b433760d33608ebaf2471b500ef996a80ad191
delta-kor <deltakbot1@gmail.com>
2024-07-20 23:15:01 +0900

    feat: add purge api cors

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,27 @@ const nextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  headers: async () => {
+    return [
+      {
+        source: '/api/cache/purge/*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
