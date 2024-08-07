5f893574afe5b8969e7681fe8b82788ba413b184
delta-kor <deltakbot1@gmail.com>
2024-08-07 22:42:54 +0900

    feat: add spark streaming

--- providers/SparkProvider.tsx
+++ providers/SparkProvider.tsx
@@ -1,13 +1,16 @@
 'use client';
 
+import useModal from '@/hooks/modal';
 import createId from '@/utils/id.util';
-import { SparkContent } from '@/utils/spark.util';
-import { ReactNode, createContext, useState } from 'react';
+import { SparkContent, SparkState, connectSpark } from '@/utils/spark.util';
+import { ReactNode, createContext, useRef, useState } from 'react';
 
 interface Context {
   chatId: string;
   history: SparkContent[];
+  response: null | string;
   send: (prompt: string) => void;
+  state: SparkState;
 }
 
 export const SparkContext = createContext<Context>({} as Context);
@@ -17,23 +20,94 @@ interface Props {
 }
 
 export default function SparkProvider({ children }: Props) {
+  const modal = useModal();
+
   const initialChatId = createId(32);
   const [chatId, setChatId] = useState<string>(initialChatId);
+  const [state, setState] = useState<SparkState>(SparkState.IDLE);
   const [history, setHistory] = useState<SparkContent[]>([]);
+  const [response, setResponse] = useState<null | string>(null);
+
+  const responseRef = useRef<null | string>(null);
+
+  function clearResponse() {
+    responseRef.current = null;
+    setResponse(responseRef.current);
+  }
 
   function handleSend(prompt: string) {
+    if (state !== SparkState.IDLE) {
+      modal.alert('Please wait for the current process to complete.');
+      return;
+    }
+
     const content: SparkContent = {
       content: prompt,
       type: 'user',
     };
 
     setHistory(prev => [...prev, content]);
+    setState(SparkState.SENDING);
+
+    const emitter = connectSpark(chatId, prompt);
+
+    emitter.on('open', () => {
+      setState(SparkState.LOADING);
+    });
+    emitter.on('close', () => {
+      setState(SparkState.IDLE);
+    });
+    emitter.on('error', () => {
+      setState(SparkState.IDLE);
+      modal.alert('An error occurred. Please try again.');
+    });
+
+    emitter.on('data', data => {
+      const type = data.type;
+
+      if (type === 'error') {
+        clearResponse();
+        setState(SparkState.IDLE);
+        modal.alert(data.message);
+      }
+
+      if (type === 'start') {
+        setState(SparkState.STREAMING);
+
+        responseRef.current = '';
+        setResponse(responseRef.current);
+      }
+
+      if (type === 'stream') {
+        responseRef.current += data.message;
+        setResponse(responseRef.current);
+      }
+
+      if (type === 'feedback') {
+        modal.alert('AI refused to generate content based on the prompt.');
+      }
+
+      if (type === 'end') {
+        if (responseRef.current === null) {
+          clearResponse();
+        } else {
+          const content: SparkContent = {
+            content: responseRef.current,
+            type: 'ai',
+          };
+          setHistory(prev => [...prev, content]);
+          clearResponse();
+        }
+      }
+    });
   }
 
   const context: Context = {
     chatId,
     history,
+    response,
     send: handleSend,
+    state,
   };
 
   return <SparkContext.Provider value={context}>{children}</SparkContext.Provider>;
--- utils/spark.util.ts
+++ utils/spark.util.ts
@@ -1,4 +1,48 @@
+import EventEmitter from 'events';
+
 export interface SparkContent {
   content: string;
   type: 'ai' | 'user';
+}
+
+export enum SparkState {
+  IDLE,
+  SENDING,
+  LOADING,
+  STREAMING,
+}
+
+export function connectSpark(chatId: string, prompt: string): EventEmitter {
+  const url = new URL(`${process.env.NEXT_PUBLIC_SPARK_URL_BASE}/run`);
+  url.searchParams.append('chatId', chatId);
+  url.searchParams.append('prompt', prompt);
+
+  const emitter = new EventEmitter();
+  const eventSource = new EventSource(url.toString());
+  eventSource.onerror = event => {
+    if (event.eventPhase === EventSource.CLOSED) {
+      eventSource.close();
+      emitter.emit('close');
+      return;
+    } else {
+      eventSource.close();
+      emitter.emit('error');
+    }
+  };
+
+  eventSource.onmessage = event => {
+    try {
+      const data = JSON.parse(event.data);
+      emitter.emit('data', data);
+    } catch (error) {
+      console.error(error);
+      emitter.emit('error');
+    }
+  };
+
+  eventSource.onopen = () => {
+    emitter.emit('open');
+  };
+
+  return emitter;
 }
