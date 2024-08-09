88685bf50e28a880146a3e861b9f7fddcd069458
delta-kor <deltakbot1@gmail.com>
2024-08-09 21:39:37 +0900

    feat: update rate limit message

---
+++ components/core/ad/AdsenseSpark.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function AdsenseSpark() {
+  useEffect(() => {
+    process.env.NEXT_PUBLIC_ENABLE_ADSENSE === '1' && renderAd();
+  }, []);
+
+  function renderAd() {
+    try {
+      // @ts-ignore
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {}
+  }
+
+  return (
+    <ins
+      data-ad-client="ca-pub-7478560982899387"
+      data-ad-format="auto"
+      data-ad-slot="1868836318"
+      data-full-width-responsive="true"
+      style={{ display: 'block', overflow: 'hidden' }}
+      className="adsbygoogle"
+    ></ins>
+  );
+}
